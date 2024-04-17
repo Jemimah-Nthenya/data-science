1c1d3f07dc2b08b06437aa7f5423e05a47cb5811
Jemimah Nthenya <jemimahogonda@gmail.com>
2024-04-17 10:20:19 +0300

    add files

---
+++ Queue.py
@@ -0,0 +1,17 @@
+from collections import deque
+countries = deque()
+countries.append('Britain')
+countries.append('Germany')
+countries.append('Canada')
+countries.append('Kenya')
+print("Initial queue")
+print(countries)
+
+print("\nElements dequeued from the queue")
+print(countries.pop())
+
+
+
+
+
+
